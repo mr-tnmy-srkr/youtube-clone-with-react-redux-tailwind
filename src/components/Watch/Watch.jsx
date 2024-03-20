2f78ba578a5109672274750faa91948c3d9013a7
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2024-03-20 18:18:39 +0530

    watch component coding under progress

--- src/components/Watch/Watch.jsx
+++ src/components/Watch/Watch.jsx
@@ -1,21 +1,126 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Avatar from "react-avatar";
+import { AiOutlineDislike, AiOutlineLike } from "react-icons/ai";
+import { BsThreeDotsVertical } from "react-icons/bs";
+import { GoDownload } from "react-icons/go";
+// import { LuSendHorizonal } from "react-icons/lu";
+import { PiShareFatLight } from "react-icons/pi";
+import { useDispatch } from "react-redux";
 import { useSearchParams } from "react-router-dom";
+// import { setMessage } from "../utils/chatSlice";
+// import LiveChat from "./LiveChat";
 
 const Watch = () => {
+  const [input, setInput] = useState("");
+  const [singleVideo, setSingleVideo] = useState(null);
   const [searchParams] = useSearchParams();
   const videoId = searchParams.get("v");
-  console.log(searchParams.get("v"));
+  const dispatch = useDispatch();
+
+  const getSingleVideo = async () => {
+    try {
+      const res = await axios.get(
+        `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}&key=${
+          import.meta.env.VITE_API_KEY
+        }`
+      );
+      setSingleVideo(res?.data?.items[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  /*  const sendMessage = () => {
+    dispatch(setMessage({ name: "Patel", message: input }));
+    setInput("");
+  }; */
+
+  useEffect(() => {
+    getSingleVideo();
+  }, []);
 
   return (
-    <div>
-      <iframe
-        width="900"
-        height="500"
-        src={`https://www.youtube.com/embed/${videoId}?&autoplay=0`}
-        title="YouTube video player"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowFullScreen
-      ></iframe>
+    <div className="flex ml-4 w-[100%] mt-2">
+      <div className="flex w-[88%]">
+        <div>
+          <iframe
+            width="900"
+            height="500"
+            src={`https://www.youtube.com/embed/${videoId}?&autoplay=0`}
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          ></iframe>
+          <h1 className="font-bold mt-2 text-lg text-left">
+            {singleVideo?.snippet?.title}
+          </h1>
+          <div className="flex items-center justify-between">
+            <div className="flex items-center justify-between w-[35%]">
+              <div className="flex">
+                <Avatar
+                  src="https://play-lh.googleusercontent.com/C9CAt9tZr8SSi4zKCxhQc9v4I6AOTqRmnLchsu1wVDQL0gsQ3fmbCVgQmOVM1zPru8UH=w240-h480-rw"
+                  size={35}
+                  round={true}
+                />
+                <h1 className="font-bold ml-2">
+                  {singleVideo?.snippet?.channelTitle}
+                </h1>
+              </div>
+              <button className="px-4 py-1 font-medium bg-black text-white rounded-full">
+                Subscribe
+              </button>
+            </div>
+            <div className="flex items-center w-[40%] justify-between mt-2">
+              <div className="flex items-center cursor-pointer bg-gray-200 px-4 py-2 rounded-full">
+                <AiOutlineLike size="20px" className="mr-5" />
+                <AiOutlineDislike size="20px" />
+              </div>
+              <div className="flex items-center cursor-pointer bg-gray-200 px-4 py-2 rounded-full">
+                <PiShareFatLight size="20px" className="mr-2" />
+                <span>Share</span>
+              </div>
+              <div className="flex items-center cursor-pointer bg-gray-200 px-4 py-2 rounded-full">
+                <GoDownload />
+                <span>Download</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="w-[100%] border border-gray-300 ml-8 rounded-lg h-fit p-4">
+          <div className="flex justify-between items-center">
+            <h1>Top Chat</h1>
+            <BsThreeDotsVertical />
+          </div>
+          <div className="overflow-y-auto h-[28rem] flex flex-col-reverse">
+            {/* <LiveChat /> */}
+          </div>
+
+          <div className="flex items-center justify-between border-t p-2">
+            <div className="flex items-center w-[90%]">
+              <div>
+                <Avatar
+                  src="https://play-lh.googleusercontent.com/C9CAt9tZr8SSi4zKCxhQc9v4I6AOTqRmnLchsu1wVDQL0gsQ3fmbCVgQmOVM1zPru8UH=w240-h480-rw"
+                  size={35}
+                  round={true}
+                />
+              </div>
+              <input
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                className="border-b border-gray-300 outline-none ml-2"
+                type="text"
+                placeholder="Send message..."
+              />
+              <div className="bg-gray-200 cursor-pointer p-2 rounded-full">
+                {/* <LuSendHorizonal onClick={sendMessage} /> */}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
+
 export default Watch;
